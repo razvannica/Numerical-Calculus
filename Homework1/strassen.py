91fe00be1bb707a73630ea82078149537f38578e
Razvan <razvan.nica30@gmail.com>
2018-03-12 15:39:45 +0200

    implemented reading from file

--- Homework1/strassen.py
+++ Homework1/strassen.py
@@ -1,5 +1,43 @@
 from math import ceil, log
 
+def read_from_file(filename):
+    lines = open(filename, 'r').read()
+    A = []
+    B = []
+    data = [item.split() for item in lines.split('\n')[:-1]]
+
+    size = int(data[0][0])
+    lower_limit = int(data[1][0])
+    data.pop(0)
+    data.pop(0)
+    i = 0
+    j = 0
+    ok = 0
+    for line in data:
+        if i <= 9 and ok == 0:
+            if line:
+                for index in range(size):
+                    if index==0:
+                        A.append([])
+                    A[i]+=line[index]
+                i+= 1
+        else:
+            ok = 1
+            if line:
+                for index in range(size):
+                    if index==0:
+                        B.append([])
+                    B[j]+=line[index]
+                j+= 1
+
+    for line in range(size):
+        for column in range(size):
+            A[line][column] = int(A[line][column])
+            B[line][column] = int(B[line][column])
+
+    return A, B, size, lower_limit
+
+
 def normalize_matrix(matrix, upper_bound):
     size = len(matrix)
     for line in range(0, len(matrix)):
@@ -9,12 +47,14 @@ def normalize_matrix(matrix, upper_bound):
         matrix.append(list(0 for j in range(0, upper_bound)))
     return matrix
 
+
 def check_if_power_of_two(no):
     if float(no / 2) > 1.0:
         check_if_power_of_two(no / 2)
     if no == 1.0:
         return 1
     return 0
+
 
 def matrix_multiplication(A, B):
     size = len(A)
@@ -25,6 +65,7 @@ def matrix_multiplication(A, B):
                 C[line][index] += A[line][column] * B[column][index]
     return C
 
+
 def matrix_divide(A, B):
     size = len(A)
     C = [[0 for j in range(0, size)] for i in range(0, size)]
@@ -33,17 +74,19 @@ def matrix_divide(A, B):
             C[line][column] = A[line][column] - B[line][column]
     return C
 
+
 def matrix_add(A, B):
     size = len(A)
     C = [[0 for j in range(0, size)] for i in range(0, size)]
     for line in range(0, size):
         for column in range(0, size):
-            C[line][column] = A[line][column] + B[line][column]
+            C[line][column] = int(A[line][column]) + int(B[line][column])
     return C
 
-def computeP(A, B):
-    size = len(A)
-    new_size = int(size / 2)
+
+def computeP(A, B, size, lower_limit):
+    n = len(A)
+    new_size = int(n / 2)
     A11 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
     A12 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
     A21 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
@@ -66,13 +109,13 @@ def computeP(A, B):
             B21[line][column] = B[line + new_size][column]
             B22[line][column] = B[line + new_size][column + new_size]
 
-    P1 = strassen_recursive(matrix_add(A11, A22), matrix_add(B11, B22))
-    P2 = strassen_recursive(matrix_add(A21, A22), B11)
-    P3 = strassen_recursive(A11, matrix_divide(B12, B22))
-    P4 = strassen_recursive(A22, matrix_divide(B21, B11))
-    P5 = strassen_recursive(matrix_add(A11, A12), B22)
-    P6 = strassen_recursive(matrix_divide(A21, A11), matrix_add(B11, B12))
-    P7 = strassen_recursive(matrix_divide(A12, A22), matrix_add(B21, B22))
+    P1 = strassen_recursive(matrix_add(A11, A22), matrix_add(B11, B22),size, lower_limit)
+    P2 = strassen_recursive(matrix_add(A21, A22), B11,size,lower_limit)
+    P3 = strassen_recursive(A11, matrix_divide(B12, B22),size,lower_limit)
+    P4 = strassen_recursive(A22, matrix_divide(B21, B11),size,lower_limit)
+    P5 = strassen_recursive(matrix_add(A11, A12), B22,size,lower_limit)
+    P6 = strassen_recursive(matrix_divide(A21, A11), matrix_add(B11, B12),size,lower_limit)
+    P7 = strassen_recursive(matrix_divide(A12, A22), matrix_add(B21, B22),size,lower_limit)
 
     C11 = matrix_add(matrix_divide(matrix_add(P1, P4), P5), P7)
     C12 = matrix_add(P3, P5)
@@ -88,52 +131,31 @@ def computeP(A, B):
             C[line + new_size][column + new_size] = C22[line][column]
     return C
 
-def strassen_recursive(A, B):
-    if len(A) == 2 and len(B) == 2:
+
+def strassen_recursive(A, B, size, lower_limit):
+    if len(A) <= lower_limit and len(B) <= lower_limit:
         return matrix_multiplication(A, B)
     else:
-        return computeP(A, B)
+        return computeP(A, B, size, lower_limit)
 
-def strassen(A, B):
-    size = len(A)
+
+def strassen(A, B, size, lower_limit):
 
     if check_if_power_of_two(size) != 1:
-        nextPowerOfTwo = lambda n: 2 ** int(ceil(log(n, 2)))
-        if size < nextPowerOfTwo(size):
-            A = normalize_matrix(A, nextPowerOfTwo(size))
-            B = normalize_matrix(B, nextPowerOfTwo(size))
+        next_power_of_two = lambda n: 2 ** int(ceil(log(n, 2)))
+        if size < next_power_of_two(size):
+            A = normalize_matrix(A, next_power_of_two(size))
+            B = normalize_matrix(B, next_power_of_two(size))
 
-    auxC = strassen_recursive(A, B)
+    auxC = strassen_recursive(A, B, len(A),lower_limit)
     C = [[0 for i in range(size)] for j in range(size)]
     for line in range(size):
         for column in range(size):
             C[line][column] = auxC[line][column]
     return C
 
+A, B, size, lower_limit = read_from_file("matrix_input.in")
 
-matrix = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-          ]
-matrix2 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
-
-result = strassen(matrix, matrix2)
+result = strassen(A, B, size, lower_limit)
 for i in range(0, len(result)):
     print(result[i])
-
